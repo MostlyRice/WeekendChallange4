7cd3302771708c6de9705dffda481036894376b4
Zygy93 <amazinzygy@gmail.com>
2018-02-25 13:14:18 -0600

    updated Database

--- database.sql
+++ database.sql
@@ -1,5 +1,4 @@
 -- create table
--- create table
 CREATE TABLE pictures (
 	id SERIAL PRIMARY KEY,
 	name varchar(255),
@@ -10,10 +9,14 @@ CREATE TABLE pictures (
 -- Add some data
 INSERT INTO pictures (name, description, pic)
 VALUES
-('Dota2 ', 'Anyone who knows me, knows Im obsessed with playing Dota 2. I signed up for the 2017 Reddit gift exchange and my secret Santa got me this vinyl figure of my favorite hero Slark! This gift was awesome —hook, line and sinker!', '/assets/dota.jpg'),
+('Dota 2 ', 'Dota 2 is a free-to-play multiplayer online battle arena (MOBA) video game developed and published by Valve Corporation. The game is the stand-alone sequel to Defense of the Ancients (DotA), which was a community-created mod for Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne.
 
-('Pho', 'Food is culture. For me as a first-generation Vietnamese/Chinese-American, comfort food is a big bowl of pho: A Vietnamese noodle soup, traditionally made with beef or chicken broth that is flavored with various spices, cuts of meat, and topped with various herbs.', '/assets/pho.jpg'),
+Anyone who knows me, knows Im obsessed with playing Dota 2. I signed up for the 2017 Reddit gift exchange and my secret Santa got me this vinyl figure of my favorite hero Slark! This gift was awesome —hook, line and sinker!', '/assets/dota.jpg'),
 
-('UNAVSA', 'My first national level conference was an amazing experience and I am so glad that I was part of it. Ive met so many other people and its amazing how much memories were made in only 4 days. The Waluigi family, brothers of Room 457, and the beautiful attendees from other regions definitely lit the way for me. ', '/assets/UNAVSA.jpg'),
+('I am Vietnamese', 'Food is culture. For me as a first-generation Vietnamese/Chinese-American, comfort food is a big bowl of pho: A Vietnamese noodle soup, traditionally made with beef or chicken broth that is flavored with various spices, cuts of meat, and topped with various herbs', '/assets/pho.jpg'),
 
-('VSAM', 'Who would have thought that we would have been such close friends within the past 9 months of just working together? I can say with absolute confidence that this show turned out to be stellar performance (and exceeded my expectations) despite having lots of challenges and setbacks. I was enchanted and wowed by our beautiful models, actors, performers, and dancers even though I was backstage with my amazing logistics team. This is probably one of my favorite pictures and memory of my friend Alison and I. She was too emotional (and ready to cry) to give a speech to our Logistics team when one of the head coordinators was going around making the other coordinators give one so she had me do it.','/assets/VSAM.jpg');
+('Ohio Squad', 'The one Vietnamese Student Assoication community that I will always hold close to my heart will be the lovely people of Ohio. When I was invited to their VSA winter social event in 2016, a few attendees were flabergasted when I introduced myself and said that I was from Minnesota. After a few icebreaker games, dinner, and drunk karaoke; it was like I was always part of their family. Subsequiently, I would always be invited to their events and I was nicknamed the "Ohio Honorary". This picture was taken from our Cabrewing 2017: Round 2 trip that we did in the summer of 2017', '/assets/Ohio.jpg'),
+
+('VSAM 2017 Tet Show', 'Lunar New year is one of my favorite holidays. I am always excited for the food and lucky money that comes with it, but during 2017 I was also part of VSAM`s annual Tet show! This is probably one of my favorite pictures and memory of my friend and Co-Logistics coordinator partner Alison and I. She was too emotional (and ready to cry) to give a speech to our Logistics team when one of the head coordinators was going around making the other coordinators give one (Womp, so she had me do it). Who would have thought that we would have been such close friends within the past 9 months of just working together? I can say with absolute confidence that this show turned out to be stellar performance (and exceeded my expectations) despite having lots of challenges and setbacks. I was enchanted and wowed by our beautiful models, actors, performers, and dancers even though I was backstage with my amazing logistics team','/assets/VSAM.jpg'),
+
+('UNAVSA', 'The Union of North American Vietnamese Student Associations’ Annual Conference is a premier event that has brought people from various backgrounds finding purpose in community service through education, philanthropy, culture and leadership. This was my first national level conference and it was an amazing experience. I am so glad that I was part of this huge event. Ive met so many other people from different VSA regions and its amazing how much memories were made in only 4 days. The Waluigi family, brothers of Room 457, and the beautiful attendees from other regions definitely lit the way for me', '/assets/UNAVSA.jpg');
